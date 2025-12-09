0ec3848ca52b4b5404c0a2a42874cc7342fdbc45
deluwarhoisn <babuhossen301@gmail.com>
2025-12-09 22:06:58 +0600

    feat: implement axios interceptor for secure API requests with user authentication

--- src/hooks/useAxiosSecure.js
+++ src/hooks/useAxiosSecure.js
@@ -1,12 +1,20 @@
 import axios from 'axios';
-import React from 'react';
+import { useEffect } from 'react';
+import useAuth from './useAuth';
+
 
 const axiosSecure = axios.create({
     baseURL: 'http://localhost:5000'
 })
 
 const useAxiosSecure = () => {
-    
+    const { user } = useAuth();
+    useEffect(() => {
+        axiosSecure.interceptors.request.use(config => {
+            config.headers.Authorization = `Bearer ${user.accessToken}`
+        })
+    }, [user])
+
     return axiosSecure;
 };
 
